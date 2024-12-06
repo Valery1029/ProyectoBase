21a7a529e1f428558e4badcb8ff4cdca4904ee5c
Valery Escobar <julisco2410@gmail.com>
2024-12-05 21:03:49 -0500

    Primera carpeta

--- WebAppApiMoto/obj/Debug/net7.0/WebAppApiMoto.AssemblyInfo.cs
+++ WebAppApiMoto/obj/Debug/net7.0/WebAppApiMoto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppApiMoto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+803f262022baf24b5bfc8ac0c665bf58dc546985")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04a75e0d48406bfa16d6d468688342f1167c1419")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppApiMoto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppApiMoto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
