49cfeb60f934b8da74999706286b701bea6596e1
Valery Escobar <julisco2410@gmail.com>
2024-12-06 10:58:26 -0500

    Creación clase AuthorizeAttribute

---
+++ WebAppApi00/Helpers/AuthorizeAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using WebAppApi00.Model;
+
+namespace WebAppApi00.Helpers
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = (User?)context.HttpContext.Items["User"];
+            if (user == null)
+            {
+                // Si el usuario no está autenticado, retorna un error 401
+
+                context.Result = new JsonResult(new { message = "Unauthorized"})
+                {
+                    StatusCode = StatusCodes.Status401Unauthorized
+                };
+            }
+        }
+    }
+}
+
