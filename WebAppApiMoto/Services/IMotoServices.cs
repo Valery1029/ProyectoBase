66b79ddfb42bc58854199606fb32440c832c0351
Valery Escobar <julisco2410@gmail.com>
2024-12-05 20:50:28 -0500

    Interfaz IMotoServices

---
+++ WebAppApiMoto/Services/IMotoServices.cs
@@ -0,0 +1,10 @@
+﻿using WebAppApiMoto.Model;
+namespace WebAppApiMoto.Services
+{
+    public interface IMotoService
+    {
+        List<Moto> GetAllMotos(bool? isActive); Moto? GetMotoByID(int id);
+        Moto AddMoto(AddUpdateMoto obj);
+        Moto? UpdateMoto(int id, AddUpdateMoto obj); bool DeleteMotoByID(int id);
+    }
+}
--- WebAppApiMoto/obj/Debug/net7.0/WebAppApiMoto.AssemblyInfo.cs
+++ WebAppApiMoto/obj/Debug/net7.0/WebAppApiMoto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppApiMoto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04a75e0d48406bfa16d6d468688342f1167c1419")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97840acef90f4014a84d23cdb66510998cef7ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppApiMoto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppApiMoto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
