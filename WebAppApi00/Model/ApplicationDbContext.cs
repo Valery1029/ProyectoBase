286f104894003ca427ccf67fe10edbed4029569e
Valery Escobar <julisco2410@gmail.com>
2024-12-06 10:20:23 -0500

    Creación clase ApplicationDbContext

---
+++ WebAppApi00/Model/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace WebAppApi00.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; } = null!; 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(new User
+                {
+                    Id = 1,
+                    FirstName = "Admin",
+                    Lastname = "System",
+                    Username = "admin",
+                    Password = "admin",
+                    IsActive = true
+                }
+            );
+        }
+    }
+}
+
--- WebAppApi00/obj/Debug/net8.0/WebAppApi00.AssemblyInfo.cs
+++ WebAppApi00/obj/Debug/net8.0/WebAppApi00.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppApi00")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a82a3b099be187192986d13defc16a2c5ccd5527")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02af681c1f225d3f8fb80e34c9433f6c2f7f03e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppApi00")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppApi00")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
