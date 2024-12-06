e14702a12e9cb8013225dd05cf8ec660e6d9b809
Valery Escobar <julisco2410@gmail.com>
2024-12-05 21:52:30 -0500

    Prueba lista

--- WebAppApiMoto/obj/Debug/net7.0/WebAppApiMoto.AssemblyInfo.cs
+++ WebAppApiMoto/obj/Debug/net7.0/WebAppApiMoto.AssemblyInfo.cs
@@ -14,15 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAppApiMoto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66b79ddfb42bc58854199606fb32440c832c0351")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21a7a529e1f428558e4badcb8ff4cdca4904ee5c")]
->>>>>>> Rama1
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a0fd8ece62d8aa8d4f0f95dea3ad1477fed163")]
->>>>>>> Rama1
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a20ed0de5c60f51fbca988ffeebc2ab3b7402b55")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAppApiMoto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAppApiMoto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
